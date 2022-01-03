143d5c1bdbe493f23be20a0c7ee64eefc871b57d
skittlemittle <cautiouspotato@gmail.com>
2022-01-03 12:15:47 +0530

    make notes folder if its missing

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import App from "./components/App";
 import { AppTheme } from "./theme";
 import { ThemeProvider } from "styled-components";
+import { loadFolder, newFolder } from "./fileOperations";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -15,6 +16,18 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// try to make the notes folder
+try {
+  loadFolder("smiling_man_dimension").catch((e) => {
+    console.log(`FileWarn:  ${e}`);
+    newFolder("smiling_man_dimension").catch((e) =>
+      console.warn("FileError:  couldnt make notes folder")
+    );
+  });
+} catch (error) {
+  console.warn("nae file operations");
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
